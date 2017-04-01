348d4afe0015b43051ae1f020d76756eddc05060
user <user@10.0.0.28>
2017-04-01 21:04:55 +0800

    solution 8

---
+++ LeetCode/src/com/john/leetcode/app/Solution8.java
@@ -0,0 +1,51 @@
+package com.john.leetcode.app;
+
+public class Solution8 {
+
+	public int myAtoi(String str) {
+		int result = 0;
+		int index = 0;
+		int sighn = 1;
+
+		// 1. empty string
+		if (str.length() == 0) {
+			return 0;
+		}
+
+		// 2. remove space
+		while (index < str.length() && str.charAt(index) == ' ') {
+			index++;
+		}
+
+		// 3. handle sign
+		if (str.charAt(index) == '+' || str.charAt(index) == '-') {
+			if (str.charAt(index) == '-') {
+				sighn = -1;
+			}
+
+			index++;
+		}
+
+		// 4. covert number
+		while (index < str.length()) {
+			int digit = str.charAt(index) - '0';
+			System.out.println("digit=" + digit);
+
+			// 5. over flow
+			if (result > Integer.MAX_VALUE / 10 || result == Integer.MAX_VALUE && digit > Integer.MAX_VALUE % 10) {
+				if (sighn == 1) {
+					return Integer.MAX_VALUE;
+				} else {
+					return Integer.MIN_VALUE;
+				}
+			}
+
+			result = 10 * result + digit;
+			index++;
+		}
+
+		return result * sighn;
+
+	}
+
+}
