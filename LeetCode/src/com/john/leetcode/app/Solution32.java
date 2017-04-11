840caa178564b3aa948432d5413e0a06969fa4ee
wanyy257 <wanyy257@cwbrnjs5n-18.UniNet.unisa.edu.au>
2017-04-11 15:13:48 +0930

    qq

--- LeetCode/src/com/john/leetcode/app/Solution32.java
+++ LeetCode/src/com/john/leetcode/app/Solution32.java
@@ -2,6 +2,7 @@ package com.john.leetcode.app;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 32. Longest Valid Parentheses. Given a string containing just the characters
@@ -18,6 +19,24 @@ import java.util.List;
  *
  */
 public class Solution32 {
+
+	public int longestValidParentheses2(String s) {
+		Stack<Integer> stack = new Stack<>();
+		int max = 0;
+		stack.push(-1);
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == ')' && stack.size() > 1 && s.charAt(stack.peek()) == '(') {
+				stack.pop();
+				max = Math.max(max, i - stack.peek());
+			} else {
+				stack.push(i);
+			}
+		}
+
+		return max;
+
+	}
 
 	public int longestValidParentheses(String s) {
 		int result = 0;
@@ -40,11 +59,14 @@ public class Solution32 {
 					System.out.println("stack.peekLast()=" + stack.peekLast());
 					stack.removeLast();
 					temp = temp + 2;
+					resultSet.add(temp);
+					System.out.println(" current temp=" + temp);
 
 				} else {
 					System.out.println("adding temp=" + temp + " to resultSet");
 					resultSet.add(temp);
 					temp = 0;
+					System.out.println(" current temp=" + temp);
 				}
 
 				break;
@@ -54,7 +76,12 @@ public class Solution32 {
 			}
 		}
 
-		resultSet.add(temp);
+		System.out.println("loop finish current temp=" + temp);
+
+		if (stack.isEmpty()) {
+			System.out.println("adding last temp");
+			resultSet.add(temp);
+		}
 
 		for (Integer i : resultSet) {
 			System.out.println("result=" + result + ", i=" + i);
@@ -63,4 +90,5 @@ public class Solution32 {
 
 		return result;
 	}
+
 }
