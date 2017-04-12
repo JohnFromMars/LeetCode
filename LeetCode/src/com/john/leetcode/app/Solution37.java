f794a520eb4bbd0d2a3ee7712e2f057d58c4fc87
wanyy257 <wanyy257@cwbrnjs5n-16.UniNet.unisa.edu.au>
2017-04-12 15:51:09 +0930

    qq

---
+++ LeetCode/src/com/john/leetcode/app/Solution37.java
@@ -0,0 +1,18 @@
+package com.john.leetcode.app;
+
+/**
+ * 37. Sudoku Solver. Write a program to solve a Sudoku puzzle by filling the
+ * empty cells.
+ * 
+ * Empty cells are indicated by the character '.'.
+ * 
+ * You may assume that there will be only one unique solution.
+ * 
+ * @author wanyy257
+ *
+ */
+public class Solution37 {
+	public void solveSudoku(char[][] board) {
+
+	}
+}
