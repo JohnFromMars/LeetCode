2a22ece00d33cdd324df7fe6820feb8089643f47
wanyy257 <wanyy257@cwbrnjs5n-17.UniNet.unisa.edu.au>
2017-04-24 14:50:21 +0930

    qq

--- LeetCode/src/com/john/leetcode/app/Solution46.java
+++ LeetCode/src/com/john/leetcode/app/Solution46.java
@@ -1,12 +1,23 @@
 package com.john.leetcode.app;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 46. Permutations. Given a collection of distinct numbers, return all possible
+ * permutations.
+ * 
+ * For example, [1,2,3] have the following permutations: [ [1,2,3], [1,3,2],
+ * [2,1,3], [2,3,1], [3,1,2], [3,2,1] ]
+ * 
+ * @author wanyy257
+ *
+ */
 public class Solution46 {
 	public List<List<Integer>> permute(int[] nums) {
 		List<List<Integer>> result = new LinkedList<>();
-		
+
 		backTrack(result, new LinkedList<>(), nums);
 
 		return result;
@@ -14,7 +25,7 @@ public class Solution46 {
 
 	private void backTrack(List<List<Integer>> result, List<Integer> temp, int[] nums) {
 		if (temp.size() == nums.length) {
-			result.add(temp);
+			result.add(new ArrayList<>(temp));
 
 		} else {
 
