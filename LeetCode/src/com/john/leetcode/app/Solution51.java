825d33ec689ff64b83a3af96642c422da7ca1c55
wanyy257 <wanyy257@cwbrnjs5n-05.UniNet.unisa.edu.au>
2017-04-26 19:39:34 +0930

    qq

---
+++ LeetCode/src/com/john/leetcode/app/Solution51.java
@@ -0,0 +1,39 @@
+package com.john.leetcode.app;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 51. N-Queens. The n-queens puzzle is the problem of placing n queens on an
+ * n×n chessboard such that no two queens attack each other.Given an integer n,
+ * return all distinct solutions to the n-queens puzzle.
+ * 
+ * Each solution contains a distinct board configuration of the n-queens'
+ * placement, where 'Q' and '.' both indicate a queen and an empty space
+ * respectively.
+ * 
+ * For example, There exist two distinct solutions to the 4-queens puzzle:
+ * 
+ * [ [".Q..", // Solution 1 "...Q", "Q...", "..Q."],
+ * 
+ * ["..Q.", // Solution 2 "Q...", "...Q", ".Q.."] ]
+ * 
+ * @author wanyy257
+ *
+ */
+public class Solution51 {
+	public List<List<String>> solveNQueens(int n) {
+		List<List<String>> result = new LinkedList<>();
+
+		return result;
+
+	}
+
+	private void solveNQueens() {
+
+	}
+
+	private void isValid(List<String> chess,int x, int y){
+    	
+    }
+}
