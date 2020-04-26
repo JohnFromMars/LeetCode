20a5d56afcf62987fcd24d8ef773669b07e2a495
JohnFromMars <rock13810@gmail.com>
2020-04-26 20:41:17 +0800

    test

--- LeetCode/src/com/john/leetcode/app/Exam.java
+++ LeetCode/src/com/john/leetcode/app/Exam.java
@@ -9,7 +9,7 @@ public class Exam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		System.out.println(test(1));
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 		String s = run("11");
 		System.out.println(s);
 		
